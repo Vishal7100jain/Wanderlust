bd28aabb02a16ddcf106b461383eecf05d7e4704
Janahvi Gaikwad <janhavigaikwad08@gmail.com>
2024-06-29 16:20:36 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema =new Schema({
+    title: 
+    {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: 
+    {
+        type: String,
+        default: "https://unsplash.com/photos/a-woman-standing-on-top-of-a-lush-green-hillside-5CbXU4G1O0c",
+        set: (v) => 
+        v ==="" 
+        ? "https://unsplash.com/photos/a-woman-standing-on-top-of-a-lush-green-hillside-5CbXU4G1O0c"
+        :v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        },
+    ],
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing)
+        {
+            await Review.deleteMany({_id: {$in : listing.reviews}});
+        }
+});
+
+const Listing  = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
