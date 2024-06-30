c09e07d1d7feb12b1fbd72e0e732cd2157867ac0
Vishal <Vishaljain7100@gmail.com>
2024-06-30 13:49:08 +0530

    solved the login error

--- routes/user.js
+++ routes/user.js
@@ -5,54 +5,56 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup",(req,res) => {
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
 router.post(
-    "/signup", 
-    wrapAsync(async(req,res) => {
-    try{
-    let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registeredUser = await User.register(newUser,password);
-    console.log(registeredUser);
-    req.login(registeredUser, (err) => {
-        if(err)
-            {
-                return next(err);
-            }
-    })
-    req.flash("success","Welcom to Wanderlust!");
-    res.redirect("/listings");
-    }
-    catch(e){
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-}));
+    "/signup",
+    wrapAsync(async (req, res) => {
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ email, username });
+            const registeredUser = await User.register(newUser, password);
+            console.log(registeredUser);
+            req.login(registeredUser, (err) => {
+                if (err) {
+                    return next(err);
+                }
+            })
+            req.flash("success", "Welcom to Wanderlust!");
+            res.redirect("/listings");
+        }
+        catch (e) {
+            req.flash("error", e.message);
+            res.redirect("/signup");
+        }
+    }));
 
-router.get("/login",(req,res) => {
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
 router.post(
     "/login",
     saveRedirectUrl,
-    passport.authenticate("local",{failureRedirect: '/login', failureFlash:true}),
-    async(req,res) => {
-    req.flash("success","Welcome back to Wanderlust!");
-    res.redirect(res.locals.redirectUrl);
-});
+    passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to Wanderlust!");
+        if (res.locals.redirectUrl) {
+            return res.redirect(res.locals.redirectUrl);
+        }
+        res.redirect("/listings")
 
-router.get("/logout",(req,res,next) => {
+    });
+
+router.get("/logout", (req, res, next) => {
     req.logout((err) => {
-        if(err)
-            {
-               return next(err);
-            }
-            req.flash("success", "You're logged out");
-            res.redirect("/listings");
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You're logged out");
+        res.redirect("/listings");
     })
 })
 
